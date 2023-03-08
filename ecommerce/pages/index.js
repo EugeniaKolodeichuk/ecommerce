9e7faf21cbadc279aabc46dac9c9f0a9e8d1ef64
Eugenia Kolodeichuk <80124589+EugeniaKolodeichuk@users.noreply.github.com>
2023-03-08 16:53:32 +0200

    implement Navbar, Footer, Hero components and Layout

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -5,14 +5,18 @@ import { Product, FooterBanner, HeroBanner } from '../components';
 const Home = ({ products, bannerData }) => {
   return (
     <div>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
       {console.log('bannerData', bannerData)}
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
-      <div className="products-container">{products?.map(product => product.name)}</div>
-      <FooterBanner />
+      <div className="products-container">
+        {products?.map(product => (
+          <Product key={product._id} product={product} />
+        ))}
+      </div>
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </div>
   );
 };
