dd7d8978f46c95be9e98ab1e615e9b764d53121c
Eugenia Kolodeichuk <80124589+EugeniaKolodeichuk@users.noreply.github.com>
2023-04-03 16:28:53 +0300

    fix hero img for mobile

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -4,7 +4,7 @@ const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    console.log('cartItems', req.body);
+    //console.log('cartItems', req.body);
 
     try {
       const params = {
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ const Home = ({ products, bannerData }) => {
   return (
     <div>
       <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
-      {console.log('bannerData', bannerData)}
+      {/* {console.log('bannerData', bannerData)} */}
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
